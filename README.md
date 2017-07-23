a6ba960a61f5150b3646b8a74997dc6bee93941c
Shramee Srivastav <shramee.srivastav@gmail.com>
2017-07-23 21:02:10 +0530

    Init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# gulp-p5
-Starter for p5 with gulp serving, live reloading changes and compiling Babel and SASS
+# P5.js starter powered by gulp to use SASS and Babel (ES6)
+
+Includes Gulp to compile Sass into CSS, along with Autoprefixer, Babel into ES5 JS and Browser Sync.
+
+Creates p5 canvas and puts in #canvas-wrap.
+
+## Setup
+
+Before starting, you'll need [Node](https://nodejs.org/) (which includes NPM).
+
+If using OSX, I'd recommend this guide to [getting set up with Node and NPM](http://www.johnpapa.net/how-to-use-npm-global-without-sudo-on-osx/).
+
+Then install Gulp using `npm install -g gulp`. This installs Gulp globally and is needed later.
+
+Clone this repo to your local computer using this command:
+
+    git clone https://git@github.com/shramee/gulp-sass-babel.git starter
+
+With the files downloaded, navigate to your `starter` folder on the command line (or Terminal) and run `npm install` to set things up.
+
+If that doesn't work, it may be necessary to use `sudo npm install`.
+
+With this set up, you should now be able to run:
+
+    gulp
+
+This will process any Sass (SCSS) files, translate ES6 to ES5 and launch a web browser showing the current files. Making changes to the files should result in the page updating automatically.
+
+## Questions
+
+Drop me a line at [shramee.srivastav@gmail.com](shramee.srivastav@gmail.com) with any questions or thoughts. Pull requests welcomed!
+
+### License
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+var
+	gulp = require( 'gulp' ),
+	browserSync = require( 'browser-sync' ),
+	$ = require( 'gulp-load-plugins' )( {lazy: true} );
+
+gulp.task( 'styles', function () {
+	return gulp
+		.src( './src/sass/**/*.scss' )
+		.pipe( $.sass().on( 'error', $.sass.logError ) )
+		.pipe( $.autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+		.pipe( $.cleanCss() )
+		.pipe( gulp.dest( 'public/css' ) )
+		.pipe( browserSync.reload( {stream: true} ) );
+} );
+
+gulp.task('vendorScripts', function() {
+	gulp.src('./src/js/vendor/**/*.js')
+			.pipe(gulp.dest('public/js/vendor'));
+});
+
+gulp.task( 'scripts', function () {
+	return gulp
+		.src( [
+			'./src/js/!(vendor)**/!(app)*.js',
+			'./src/js/app.js'
+		] )
+		.pipe( $.plumber() )
+		.pipe( $.babel( {
+			presets: ['es2015']
+		} ) )
+		.pipe( $.concat( 'app.js' ) )
+		.pipe( $.uglify() )
+		.pipe( gulp.dest( 'public/js' ) )
+		.pipe( browserSync.reload( {stream: true} ) );
+} );
+
+// Optimizes the images that exists
+gulp.task( 'images', function () {
+	return gulp
+		.src( 'src/images/**' )
+		.pipe( $.changed( 'images' ) )
+		.pipe( $.imagemin( {
+			// Lossless conversion to progressive JPGs
+			progressive: true,
+			// Interlace GIFs for progressive rendering
+			interlaced: true
+		} ) )
+		.pipe( gulp.dest( 'public/images' ) )
+		.pipe( $.size( {title: 'images'} ) );
+} );
+
+gulp.task( 'html', function () {
+	return gulp
+		.src( './src/**/*.html' )
+		.pipe( gulp.dest( 'public/' ) )
+} );
+
+gulp.task( 'browser-sync', ['styles', 'scripts'], function () {
+	browserSync( {
+		server: {
+			baseDir: "./public/",
+			injectChanges: true // this is new
+		}
+	} );
+} );
+
+gulp.task( 'deploy', function () {
+	return gulp
+		.src( './public/**/*' )
+		.pipe( ghPages() );
+} );
+
+gulp.task( 'watch', function () {
+	// Watch .html files
+	gulp.watch( 'src/**/*.html', ['html', browserSync.reload] );
+	gulp.watch( "public/*.html" ).on( 'change', browserSync.reload );
+	// Watch .sass files
+	gulp.watch( 'src/sass/**/*.scss', ['styles', browserSync.reload] );
+	// Watch .js files
+	gulp.watch( 'src/js/*.js', ['scripts', browserSync.reload] );
+	// Watch .js files
+	gulp.watch( 'src/js/vendor/*', ['vendorScripts', browserSync.reload] );
+	// Watch image files
+	gulp.watch( 'src/images/**/*', ['images', browserSync.reload] );
+} );
+
+gulp.task( 'default', function () {
+	gulp.start(
+		'styles',
+		'vendorScripts',
+		'scripts',
+		'images',
+		'html',
+		'browser-sync',
+		'watch'
+	);
+} );
